0b6472f430eb916c5a5a5697a70cf7135a8d0ae6
toliner <toliner@toliner.dev>
2024-05-19 04:02:12 +0900

    グローバルエラーハンドラの追加

---
+++ server/src/main/kotlin/dev/toliner/todoark/server/GlobalErrorHandler.kt
@@ -0,0 +1,23 @@
+package dev.toliner.todoark.server
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Error
+
+@Controller("/errors")
+class GlobalErrorHandler {
+    @Error(exception = IllegalStateException::class)
+    fun illegalArgument(
+        exception: IllegalArgumentException
+    ): HttpResponse<ErrorResponse> {
+        return HttpResponse.badRequest<ErrorResponse>().body(ErrorResponse(
+            error = IllegalArgumentException::class.simpleName!!,
+            message = exception.message!!
+        ))
+    }
+
+    data class ErrorResponse(
+        val error: String,
+        val message: String
+    )
+}
